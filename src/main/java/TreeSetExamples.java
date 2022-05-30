6b3bfeb175bd0372c72132c14ad7f524ad5b73eb
vinoku <vinoku78@gmail.com>
2022-05-30 11:16:02 +0200

    Set

--- src/main/java/TreeSetExamples.java
+++ src/main/java/TreeSetExamples.java
@@ -4,27 +4,30 @@ import java.util.TreeSet;
 
 public class TreeSetExamples {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Set<Integer> treeSet = new TreeSet<>();
-    treeSet.add(1);
-    treeSet.add(300);
-    treeSet.add(47);
-    treeSet.add(6);
-    System.out.println(treeSet);
-    treeSet.add(6);
-    System.out.println(treeSet);
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(300);
+        treeSet.add(47);
+        treeSet.add(6);
+        System.out.println(treeSet);
+        treeSet.add(6);
+        System.out.println(treeSet);
 
-    Set<String> wordSet = new TreeSet<>(Comparator.comparing(String::length));
-    wordSet.add("tiger");
-    wordSet.add("giraffe");
-    wordSet.add("bear");
-    System.out.println(wordSet);
-    wordSet.add("wolf");
-    System.out.println(wordSet);
-    wordSet.remove("giraffe");
-    System.out.println(wordSet);
+        Set<String> wordSet = new TreeSet<>(Comparator.comparing(String::length));
+        wordSet.add("tiger");
+        wordSet.add("giraffe");
+        wordSet.add("bear");
+        System.out.println(wordSet);
+        wordSet.add("wolf");
+        System.out.println(wordSet); // wolf is not added because now we compare by length and wolf and bear have same length
+        wordSet.remove("giraffe");
+        System.out.println(wordSet);
 
-  }
+
+
+
+    }
 
 }
